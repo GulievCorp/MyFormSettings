1062ac83e1bc2b3122f052208a2e7cdcd519faf3
Anar Guliev <guliev.anar1998@gmail.com>
2021-10-19 21:27:24 +0300

    add all

---
+++ src/components/Form.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
+export default function Form() {
+  const [firstname, setFirstname] = React.useState('');
+  const [lastname, setLastname] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleClickClear = () => {
+    setFirstname('');
+    setLastname('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <div className="row">
+        <TextField
+          onChange={(e) => setFirstname(e.target.value)}
+          value={firstname}
+          className="inptTxt"
+          label="Имя"
+          fullWidth
+        />
+        <TextField
+          onChange={(e) => setLastname(e.target.value)}
+          value={lastname}
+          className="inptTxt"
+          label="Фамилия"
+          fullWidth
+        />
+      </div>
+      <div className="row">
+        <TextField
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          className="inptTxt"
+          label="E-mail"
+          fullWidth
+        />
+        <TextField
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          className="inptTxt"
+          label="Пароль"
+          fullWidth
+        />
+      </div>
+      <br />
+      <div className="row">
+        <Button className="btn" variant="contained" color="primary">
+          Зарегистрироваться
+        </Button>
+        <Button onClick={handleClickClear} className="btn" variant="contained" color="secondary">
+          Очистить
+        </Button>
+      </div>
+    </div>
+  );
+}
