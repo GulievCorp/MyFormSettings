f3e3aea83d863ebdd15795321213d9be235cb374
Anar Guliev <guliev.anar1998@gmail.com>
2021-10-21 17:44:04 +0300

    fix form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Form from "./components/Form";
+import AnyForm from "./components/AnyForm";
 
 function App() {
   return <div className="App">
       <Form />
+      <AnyForm />
   </div>;
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,44 +1,43 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+const defaultValues = {
+  firstname: '',
+  lastname: '',
+  email: '',
+  password: '',
+};
 
 export default function Form() {
-  const [firstname, setFirstname] = React.useState('');
-  const [lastname, setLastname] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-
-  const handleClickClear = () => {
-    setFirstname('');
-    setLastname('');
-    setEmail('');
-    setPassword('');
-  };
-
-  const handleClickRegister = () => {
-    const form = {
-      firstname,
-      lastname,
-      email,
-      password,
-    };
-    console.log(form);
-    handleClickClear();
-  };
-
+  const { handleSubmit, reset, register, formState } = useForm({ defaultValues });
+  const onSubmit = (values) => {
+    console.log(values);
+    reset();
+  }
   return (
     <div>
       <div className="row">
         <TextField
-          onChange={(e) => setFirstname(e.target.value)}
-          value={firstname}
+          name="firstname"
+          {...register('firstname', {
+            required: 'Это обязательное поле',
+          })}
+          helperText={formState.errors.firstname && formState.errors.firstname.message}
+          error={!!formState.errors.firstname}
           className="inptTxt"
           label="Имя"
           fullWidth
         />
         <TextField
-          onChange={(e) => setLastname(e.target.value)}
-          value={lastname}
+          {...register('lastname', {
+            required: 'Это обязательное поле',
+          })}
+          helperText={formState.errors.lastname && formState.errors.lastname.message}
+          error={!!formState.errors.lastname}
+          className="inptTxt"
+          name="lastname"
           className="inptTxt"
           label="Фамилия"
           fullWidth
@@ -46,27 +45,46 @@ export default function Form() {
       </div>
       <div className="row">
         <TextField
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
+          {...register('email', {
+            required: 'Это обязательное поле',
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: 'invalid email address',
+            },
+          })}
+          helperText={formState.errors.email && formState.errors.email.message}
+          error={!!formState.errors.email}
           className="inptTxt"
-          label="E-mail"
+          name="email"
           fullWidth
         />
         <TextField
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          type="password"
+          {...register('password', {
+            required: 'Это обязательное поле',
+          })}
+          helperText={formState.errors.password && formState.errors.password.message}
+          error={!!formState.errors.password}
           className="inptTxt"
+          name="password"
+          type="password"
           label="Пароль"
           fullWidth
         />
       </div>
       <br />
       <div className="row">
-        <Button onClick={handleClickRegister} className="btn" variant="contained" color="primary">
+        <Button
+          onClick={handleSubmit(onSubmit)}
+          className="btn"
+          variant="contained"
+          color="primary">
           Зарегистрироваться
         </Button>
-        <Button onClick={handleClickClear} className="btn" variant="contained" color="secondary">
+        <Button
+          onClick={() => reset()}
+          className="btn"
+          variant="contained"
+          color="secondary">
           Очистить
         </Button>
       </div>
